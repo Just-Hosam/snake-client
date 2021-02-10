9fa925e9380eef0c7f3cc4e029b4595942d913bd
Hosam Dahrooge <Hosam_firas@hotmail.com>
2021-02-10 21:18:04 +0000

    added exit command

--- play.js
+++ play.js
@@ -1,3 +1,25 @@
 const connect = require('./client');
+
+const handleUserInput = (key) => {
+  if (key === '\u0003') {
+    console.log('Thanks for using me, ciao!');
+    process.exit();
+  }
+};
+
+/**
+ * Setup User Interface 
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  stdin.on('data', handleUserInput);
+  return stdin;
+}
+
 console.log('Connecting ...');
 connect();
+setupInput();
